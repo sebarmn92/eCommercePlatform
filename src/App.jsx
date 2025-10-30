017d8ee5ba3b117aa527c60136d3ddaba38e1c69
sebarmn92 <129097998+sebarmn92@users.noreply.github.com>
2025-10-30 22:45:13 +0200

    initial project start

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './routes/home/home'
+import Navigation from './routes/navigation/navigation'
+
+
+
+const Shop = () => {
+
+  return (
+    <h1>I am the shop page</h1>
+  )
+}
+
+function App() {
+
+
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation/>}>
+        <Route index element={<Home />}/>
+        <Route path='shop' element={<Shop />}/>
+      </Route> 
+      
+    </Routes>
+      
+   
+  )
+}
+
+export default App
---
+++ src/routes/navigation/navigation.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link} from 'react-router-dom'
+import Logo from '../../assets/crown.svg?react'
+import './navigation.scss'
+
+const Navigation = () =>{
+  return (
+    <>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <Logo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+          <Link className='nav-link' to='/sign-in'>
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
+}
+
+export default Navigation
